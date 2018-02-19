ac936b852bee985f08b7ed811bcb0eb732e5ed9b
Brett Anwar Brock <brett.brock@brockresearch.com>
2018-02-18 22:21:08 -0500

    Adds Minimal Book Initialization Script

--- init.sh
+++ init.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env sh
 
 if [ -f README.md ]; then
-  echo 'set the Introduction README.md to the "# Introduction" header only'
+  # echo 'set the Introduction README.md to the "# Introduction" header only'
+  echo "# Introduction\n" > README.md
+  
+  if [ -f literature.bib ]; then
+    # echo 'Erase all content from literature.bib'
+    cat /dev/null > literature.bib
+
+    if [ -f book.json ]; then
+      # echo 'Run gitbook install to install plug-ins'
+      gitbook install
+    else
+      echo 'Missing file: book.json'
+      exit
+    fi
+  else
+    echo 'Missing file: literature.bib'
+    exit
+  fi
 else
   echo 'Missing file: README.md'
-  exit
-fi
-
-if [ -f literature.bib ]; then
-  echo 'Erase all content from literature.bib'
-else
-  echo 'Missing file: literature.bib'
-  exit
-fi
-
-if [ -f book.json ]; then
-  echo 'Run gitbook install to install plug-ins'
-else
-  echo 'Missing file: book.json'
   exit
 fi
